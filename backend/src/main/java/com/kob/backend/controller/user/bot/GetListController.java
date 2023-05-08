77f72aa375e5a1899c16759c4be9e93e41eaf61c
storm-crypto <1742593818@qq.com>
2023-05-08 15:29:06 +0800

    5.2 创建个人中心页面下" ;

--- backend/src/main/java/com/kob/backend/controller/user/bot/GetListController.java
+++ backend/src/main/java/com/kob/backend/controller/user/bot/GetListController.java
@@ -3,7 +3,6 @@ package com.kob.backend.controller.user.bot;
 import com.kob.backend.pojo.Bot;
 import com.kob.backend.service.user.bot.GetListService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
